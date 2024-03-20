069ea3c00cecba647ae59f31daf55e6814c84cc5
to-masa <masashi.t07@gmail.com>
2024-03-20 22:51:21 +0900

    update 著者のデータを扱うメソッドを作成

--- src/main/kotlin/com/qcbookapp/infrastructure/book/BookRepositoryImpl.kt
+++ src/main/kotlin/com/qcbookapp/infrastructure/book/BookRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.qcbookapp.infrastructure.book
 
+import com.qcbookapp.domain.model.author.AuthorId
 import com.qcbookapp.domain.model.book.Book
 import com.qcbookapp.domain.model.book.BookId
 import com.qcbookapp.domain.model.book.BookTitle
@@ -20,12 +21,12 @@ class BookRepositoryImpl(
     /**
      * 取得したデータをドメインオブジェクトにマッピングする
      */
-    private val rootMapper: (BookRow) -> Book = { result ->
+    private val rootMapper: (BookDao.BookRow) -> Book = { result ->
         result.run {
             Book.reconstruct(
                 id = BookId(id),
                 title = BookTitle(title),
-                authorId = authorId,
+                authorId = AuthorId(authorId),
                 createdAt = CreatedAt(createdAt),
                 updatedAt = UpdatedAt(updatedAt),
             )
@@ -37,5 +38,30 @@ class BookRepositoryImpl(
      */
     override fun findAll(): List<Book> {
         return bookDao.findAll().map(rootMapper)
+    }
+
+    /**
+     * IDで書籍を取得する
+     */
+    override fun findById(id: BookId): Book? {
+        return bookDao.findById(id)?.let(rootMapper)
+    }
+
+    /**
+     * 書籍を作成する
+     */
+    override fun insert(book: Book) {
+        bookDao.insert(
+            book = book
+        )
+    }
+
+    /**
+     * 書籍を更新する
+     */
+    override fun update(book: Book) {
+        bookDao.update(
+            book = book
+        )
     }
 }
--- src/main/kotlin/com/qcbookapp/useCase/book/BookRepository.kt
+++ src/main/kotlin/com/qcbookapp/useCase/book/BookRepository.kt
@@ -1,10 +1,17 @@
 package com.qcbookapp.useCase.book
 
 import com.qcbookapp.domain.model.book.Book
+import com.qcbookapp.domain.model.book.BookId
 
 /**
  * 書籍リポジトリ
  */
 abstract class BookRepository {
     abstract fun findAll(): List<Book>
+
+    abstract fun findById(id: BookId): Book?
+
+    abstract fun insert(book: Book)
+
+    abstract fun update(book: Book)
 }
