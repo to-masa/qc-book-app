cc6a908eed89d7dd14bdfd12b84e4c57b50b7052
to-masa <masashi.t07@gmail.com>
2024-03-20 22:29:18 +0900

    update APIコール時のエラーをカスタマイズした

--- src/main/kotlin/com/qcbookapp/presentation/rest/RestExceptionHandler.kt
+++ src/main/kotlin/com/qcbookapp/presentation/rest/RestExceptionHandler.kt
@@ -2,14 +2,18 @@ package com.qcbookapp.presentation.rest
 
 import com.qcbookapp.domain.model.DomainException
 import com.qcbookapp.domain.model.EntityNotFoundException
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import java.lang.Exception
 
 @RestControllerAdvice
-class RestExceptionHandler {
+class RestExceptionHandler: ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(Exception::class)
     fun handling(
@@ -32,7 +36,7 @@ class RestExceptionHandler {
         val body = ErrorResponseBody(
             error = ErrorInfo(
                 code = HttpStatus.NOT_FOUND.value(),
-                message = exception.message ?: "値が見つかりませんでした",
+                message = exception.message ?: ApiError.NOT_FOUND.message,
             ),
         )
         return ResponseEntity<ErrorResponseBody>(body, httpStatus)
@@ -46,7 +50,7 @@ class RestExceptionHandler {
         val body = ErrorResponseBody(
             error = ErrorInfo(
                 code = HttpStatus.BAD_REQUEST.value(),
-                message = exception.message ?: "エラーが発生しました",
+                message = exception.message ?: ApiError.BAD_REQUEST.message,
             ),
         )
         return ResponseEntity<ErrorResponseBody>(body, httpStatus)
@@ -65,10 +69,35 @@ class RestExceptionHandler {
         val body = ErrorResponseBody(
             error = ErrorInfo(
                 code = HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                message = "サーバーでエラーが発生しました",
+                message = ApiError.INTERNAL_SERVER_ERROR.message,
             ),
         )
         return ResponseEntity<ErrorResponseBody>(body, httpStatus)
+    }
+
+    /**
+     * APIでのエラー情報を返すレスポンスのボディ
+     * - 他の例外発生時のレスポンスの構成と統一するためにカスタマイズしている
+     */
+    override fun handleExceptionInternal(
+        exception: Exception,
+        body: Any?,
+        headers: HttpHeaders,
+        statusCode: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        val errorBody = ErrorResponseBody(
+            error = ErrorInfo(
+                code = statusCode.value(),
+                message = when (statusCode) {
+                    HttpStatus.NOT_FOUND -> ApiError.NOT_FOUND.message
+                    HttpStatus.BAD_REQUEST -> ApiError.BAD_REQUEST.message
+                    HttpStatus.INTERNAL_SERVER_ERROR -> ApiError.INTERNAL_SERVER_ERROR.message
+                    else -> ""
+                },
+            ),
+        )
+        return super.handleExceptionInternal(exception, errorBody, headers, statusCode, request)
     }
 
     /**
@@ -88,4 +117,32 @@ class RestExceptionHandler {
         val code: Int,
         val message: String,
     )
+
+    /**
+     * REST-API上で例外が発生した場合に返すエラー
+     * */
+    enum class ApiError(
+        /** エラーコード */
+        val code: String,
+        /** エラーメッセージ */
+        val message: String,
+    ) {
+        /** 見つからなかった場合に返すエラー */
+        NOT_FOUND(
+            code = HttpStatus.NOT_FOUND.value().toString(),
+            message = "見つかりませんでした",
+        ),
+
+        /** クライアント側からの不正なリクエストに返すエラー */
+        BAD_REQUEST(
+            code = HttpStatus.BAD_REQUEST.value().toString(),
+            message = "入力エラーです",
+        ),
+
+        /** サーバーエラーが発生した場合に返すエラー */
+        INTERNAL_SERVER_ERROR(
+            code = HttpStatus.INTERNAL_SERVER_ERROR.value().toString(),
+            message = "サーバーでエラーが発生しました",
+        )
+    }
 }
