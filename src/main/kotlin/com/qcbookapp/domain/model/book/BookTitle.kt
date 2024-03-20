83c2a255e5b981608b1ee08203392b7567d99aaa
to-masa <masashi.t07@gmail.com>
2024-03-20 22:52:21 +0900

    add ルールを追加した

--- src/main/kotlin/com/qcbookapp/domain/model/book/BookTitle.kt
+++ src/main/kotlin/com/qcbookapp/domain/model/book/BookTitle.kt
@@ -7,4 +7,13 @@ package com.qcbookapp.domain.model.book
 data class BookTitle(
     val value: String
 ) {
+    init {
+        if(value.length > MAX_LENGTH) {
+            throw IllegalArgumentException("書籍タイトルは${MAX_LENGTH}文字以下でなければなりません")
+        }
+    }
+    
+    companion object {
+        private const val MAX_LENGTH: Int = 100
+    }
 }
