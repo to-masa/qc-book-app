60e4a446b6e15d7fdecc4d376d18c18c2f7310f3
to-masa <masashi.t07@gmail.com>
2024-03-23 02:14:25 +0900

    update Kotest, Jooq, flyway, mockkの依存関係を追加した

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    dependencies {
+        // 最新のflywayを利用するためにはclasspathを指定する必要がある
+        classpath("org.flywaydb:flyway-database-postgresql:10.10.0")
+    }
+}
+
 plugins {
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
+    id("nu.studer.jooq") version "8.2.3"
+    id("org.flywaydb.flyway") version "10.10.0"
 }
 
 group = "com"
@@ -25,9 +34,19 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jooq")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
+    // モック用ライブラリ
+    testImplementation("io.mockk:mockk:1.13.10")
+    // kotest
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.4")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.jooq:jooq-codegen:3.19.1")
+    implementation("org.jooq:jooq:3.19.1")
+    implementation("org.jooq:jooq-meta:3.19.1")
+    runtimeOnly("org.postgresql:postgresql:42.7.3")
+    jooqGenerator("org.postgresql:postgresql:42.7.3")
+    jooqGenerator("org.jooq:jooq-codegen:3.19.1")
 }
 
 tasks.withType<KotlinCompile> {
@@ -39,4 +58,48 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+flyway {
+    url = "jdbc:postgresql://localhost:5432/qc_test"
+    user= "postgres"
+    password= "secret"
+}
+
+jooq {
+    version.set("3.19.1")
+    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
+    configurations {
+        create("main") {
+            generateSchemaSourceOnCompilation.set(true)
+
+            jooqConfiguration.apply {
+                logging = org.jooq.meta.jaxb.Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url= "jdbc:postgresql://localhost:5432/qc_test"
+                    user = "postgres"
+                    password = "secret"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "app"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                    }
+                    target.apply {
+                        packageName = "com.qcbookapp.infrastructure.jooq"
+                        directory = "build/generated-src/jooq/main"  // default (can be omitted)
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
 }
