18528c8755b142989501c9892392a49da6e95977
to-masa <masashi.t07@gmail.com>
2024-03-23 02:39:49 +0900

    update 型安全にするためにjooqのオブジェクトを用いてDB操作を行うようにした

--- src/main/kotlin/com/qcbookapp/infrastructure/author/AuthorDao.kt
+++ src/main/kotlin/com/qcbookapp/infrastructure/author/AuthorDao.kt
@@ -1,11 +1,10 @@
 package com.qcbookapp.infrastructure.author
 
-import com.qcbookapp.domain.model.author.Author
 import com.qcbookapp.domain.model.author.AuthorId
+import com.qcbookapp.infrastructure.jooq.App
+import com.qcbookapp.infrastructure.jooq.tables.records.AuthorRecord
 import org.jooq.DSLContext
-import org.jooq.impl.DSL
 import org.springframework.stereotype.Component
-import java.time.LocalDateTime
 
 /**
  * 著者テーブルとやりとりするクラス
@@ -14,46 +13,26 @@ import java.time.LocalDateTime
 class AuthorDao(
     private val dslContext: DSLContext,
 ) {
+    private val jAppSchema: App = App.APP
+    private val jAuthorTable = jAppSchema.AUTHOR
 
-    fun newRecord() = dslContext.newRecord(DSL.table("app.book"))
+    fun newRecord(): AuthorRecord = dslContext.newRecord(jAuthorTable)
 
     /**
      * 全ての著者を取得する
      */
-    fun findAll(): List<AuthorRow> {
-        return dslContext.selectFrom("app.author")
+    fun findAll(): List<AuthorRecord> {
+        return dslContext.selectFrom(jAuthorTable)
             .fetch()
-            .into(AuthorRow::class.java)
+            .into(jAuthorTable)
     }
 
-    fun findById(id: AuthorId): AuthorRow? {
-        return dslContext.selectFrom("app.author")
-            .where(DSL.field("id").eq(id.value))
+    fun fetchById(id: AuthorId): AuthorRecord? {
+        return dslContext.selectFrom(jAuthorTable)
+            .where(
+                jAuthorTable.ID.eq(id.value)
+            )
             .fetchOne()
-            ?.into(AuthorRow::class.java)
+            ?.into(jAuthorTable)
     }
-
-    fun insert(author: Author) {
-        dslContext.insertInto(DSL.table("app.author"))
-            .columns(DSL.field("id"), DSL.field("name"), DSL.field("kana"), DSL.field("created_at"), DSL.field("updated_at"))
-            .values(author.identifier.value, author.name.value, author.kana.value, author.createdAt.value, author.updatedAt.value)
-            .execute()
-    }
-
-    fun update(author: Author) {
-        dslContext.update(DSL.table("app.author"))
-            .set(DSL.field("name"), author.name.value)
-            .set(DSL.field("kana"), author.kana.value)
-            .set(DSL.field("updated_at"), author.updatedAt.value)
-            .where(DSL.field("id").eq(author.identifier.value))
-            .execute()
-    }
-
-    class AuthorRow(
-        val id: Long,
-        val name: String,
-        val kana: String,
-        val createdAt: LocalDateTime,
-        val updatedAt: LocalDateTime
-    )
 }
--- src/main/kotlin/com/qcbookapp/infrastructure/author/AuthorRepositoryImpl.kt
+++ src/main/kotlin/com/qcbookapp/infrastructure/author/AuthorRepositoryImpl.kt
@@ -6,6 +6,7 @@ import com.qcbookapp.domain.model.author.AuthorKana
 import com.qcbookapp.domain.model.author.AuthorName
 import com.qcbookapp.domain.model.shared.CreatedAt
 import com.qcbookapp.domain.model.shared.UpdatedAt
+import com.qcbookapp.infrastructure.jooq.tables.records.AuthorRecord
 import com.qcbookapp.useCase.author.AuthorRepository
 import org.springframework.stereotype.Repository
 
@@ -19,49 +20,68 @@ class AuthorRepositoryImpl(
 ) : AuthorRepository {
 
     /**
-     * 取得したデータをドメインオブジェクトにマッピングする
-     */
-    private val rootMapper: (AuthorDao.AuthorRow) -> Author = { result ->
-        result.run {
-            Author.reconstruct(
-                id = AuthorId(id),
-                name = AuthorName(name),
-                kana = AuthorKana(kana),
-                createdAt = CreatedAt(createdAt),
-                updatedAt = UpdatedAt(updatedAt),
-            )
-        }
-    }
-
-    /**
      * 全ての書籍を取得する
      */
     override fun findAll(): List<Author> {
-        return authorDao.findAll().map(rootMapper)
+        return authorDao.findAll().map { authorRecord -> authorMapping(authorRecord) }
     }
 
     /**
      * IDで書籍を取得する
      */
-    override fun findById(id: AuthorId): Author? {
-        return authorDao.findById(id)?.let(rootMapper)
+    override fun fetchById(id: AuthorId): Author? {
+        return authorDao.fetchById(id)?.let { authorRecord -> authorMapping(authorRecord) }
     }
 
     /**
      * 書籍を作成する
      */
     override fun insert(author: Author) {
-        authorDao.insert(
-            author = author
-        )
+        // 保存する
+        privateSave(author)
     }
 
     /**
      * 書籍を更新する
      */
     override fun update(author: Author) {
-        authorDao.update(
-            author = author
+        privateSave(author)
+    }
+
+    /**
+     * 書籍を保存する
+     *  - 作成の場合は新規レコードを作成する
+     *  - 更新の場合は既存レコードを更新する
+     */
+    private fun privateSave(author: Author) {
+        val record: AuthorRecord = authorDao.fetchById(author.identifier) ?: authorDao.newRecord()
+        record.run {
+            updateBy(author)
+            store()
+        }
+    }
+
+    /**
+     * レコードを更新する
+     */
+    private fun AuthorRecord.updateBy(author: Author) {
+        id = author.identifier.value
+        name = author.name.value
+        kana = author.kana.value
+        createdAt = author.createdAt.value
+        updatedAt = author.updatedAt.value
+    }
+
+    /**
+     * 取得したレコードをドメインオブジェクトにマッピングする
+     */
+    private fun authorMapping(authorRecord: AuthorRecord): Author {
+        return Author.reconstruct(
+            id = AuthorId(authorRecord.id),
+            name = AuthorName(authorRecord.name),
+            kana = AuthorKana(authorRecord.kana),
+            createdAt = CreatedAt(authorRecord.createdAt),
+            updatedAt = UpdatedAt(authorRecord.updatedAt),
         )
     }
 }
